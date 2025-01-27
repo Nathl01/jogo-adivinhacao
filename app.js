b96396e09c7b9b7374c561263fc285dd148c11e1
Michael Bullet <contato@michaelbullet.com>
2025-01-27 02:10:05 -0300

    Update app.js
    Usei const para variáveis que não mudam. <3
    
    Corrigi a função Math.random para garantir que o número seja um inteiro entre 1 e numeroMaximo. //
    
    Usei !== para comparações estritas e === para igualdade estrita.
    
    Incrementação de tentativas logo após solicitar um palpite para simplificar.

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 alert("Boas vindas ao jogo do número secreto!");
-let numeroMaximo = 1000;
-let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
-console.log(numeroSecreto);
-let palpite;
-let tentativas = 1;
 
-while (palpite != numeroSecreto) {
-  palpite = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
-  if (palpite == numeroSecreto) {
+const numeroMaximo = 1000;
+const numeroSecreto = Math.floor(Math.random() * numeroMaximo) + 1;
+console.log(numeroSecreto);
+
+let palpite;
+let tentativas = 0;
+
+while (palpite !== numeroSecreto) {
+  palpite = parseInt(prompt(`Escolha um número entre 1 e ${numeroMaximo}`));
+  tentativas++;
+
+  if (palpite === numeroSecreto) {
     break;
+  } else if (palpite > numeroSecreto) {
+    alert(`O número secreto é menor que ${palpite}`);
   } else {
-    if (palpite > numeroSecreto) {
-      alert(`O número secreto é menor que ${palpite}`);
-    } else {
-      alert(`O número secreto é maior que ${palpite}`);
-    }
-    tentativas++;
+    alert(`O número secreto é maior que ${palpite}`);
   }
 }
-let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
-alert(
-  `Parabéns, você acertou! Número Secreto: ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`
-);
+
+const palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Parabéns, você acertou! Número Secreto: ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
